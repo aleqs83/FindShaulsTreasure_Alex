c8e11747acfa94f86deda86c6f2ce4482d391c8d
Kre0ns <yonpun13@gmail.com>
2026-01-15 15:38:40 +0200

    feat: implement QuestInfo class

---
+++ Models/QuestInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindShaulsTreasure.Models
+{
+    public enum QuestType { Manual, Automatic }
+
+    class QuestInfo
+    {
+        public required string QuestName { get; set; }
+
+        public required string QuestHint { get; set; }
+        public required string QuestAnwser { get; set; }
+
+        //public required string LocationHint { get; set; }
+        //public required string QuestLocation { get; set; }
+
+        public QuestType QuestType { get; set; }
+        public bool QuestSuccess { get; set; }
+    }
+}
